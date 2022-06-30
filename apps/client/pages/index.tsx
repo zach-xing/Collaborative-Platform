9ef0c2080eca80ab6bb9aed9ccca98a1daea9d5e
Zeekg <3193373997@qq.com>
2022-06-30 23:22:56 +0800

    initial

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -1,10 +1,21 @@
-import { Button } from "ui";
+import { Button } from "@chakra-ui/react";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
-      <Button />
+      <Button colorScheme="teal" variant="solid">
+        Button
+      </Button>
+      <Button colorScheme="teal" variant="outline">
+        Button
+      </Button>
+      <Button colorScheme="teal" variant="ghost">
+        Button
+      </Button>
+      <Button colorScheme="teal" variant="link">
+        Button
+      </Button>
     </div>
   );
 }
