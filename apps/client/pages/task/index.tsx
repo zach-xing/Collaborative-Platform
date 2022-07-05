be1b6c41722bd2bab7e6d446866b23b4fbe522a6
Zeekg <3193373997@qq.com>
2022-07-05 16:08:16 +0800

    feat(client): 'drag' function

--- apps/client/pages/task/index.tsx
+++ apps/client/pages/task/index.tsx
@@ -1,63 +1,127 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Paper, Card } from "@mui/material";
 import { Container, Draggable } from "react-smooth-dnd";
 
-const list = [
-  { label: "第一个数据", fieldName: "data-a1", children: [] },
-  { label: "第二个数据", fieldName: "data-a2", children: [] },
-  { label: "第三个数据", fieldName: "data-a3", children: [] },
+const tmpList = [
+  {
+    id: 0,
+    name: "重要且紧急",
+    children: [
+      {
+        label: "one-111",
+      },
+      {
+        label: "one-222",
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: "重要不紧急",
+    children: [
+      {
+        label: "two-111",
+      },
+      {
+        label: "two-222",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "紧急不重要",
+    children: [
+      {
+        label: "three-111",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "不重要不紧急",
+    children: [
+      {
+        label: "four-111",
+      },
+    ],
+  },
 ];
+
+interface ITaskItem {
+  title: string;
+}
 
 const ContainerBox: any = Container;
 const DraggableBox: any = Draggable;
+
+// 作用就是进行某一列的真实操作（比如删除、增加）
+const applyDrag = (
+  arr: Array<{ label: string }>,
+  dropResult: any
+): Array<{ label: string }> => {
+  const { removedIndex, addedIndex, payload } = dropResult;
+  if (removedIndex === null && addedIndex === null) return arr;
+
+  const result = [...arr];
+  let itemToAdd = payload;
+
+  if (removedIndex !== null) {
+    itemToAdd = result.splice(removedIndex - 1, 1)[0];
+  }
+
+  if (addedIndex !== null) {
+    result.splice(addedIndex - 1, 0, itemToAdd);
+  }
+
+  return result;
+};
 
 /**
  * 任务页面
  */
 const Task = () => {
-  const [data, setData] = React.useState<any[]>(list);
+  const [data, setData] = React.useState<any>(tmpList);
 
-  const onDrag = (arr: any[] = [], dragResult: any) => {
-    const { removedIndex, addedIndex, payload } = dragResult;
-    if (removedIndex === null && addedIndex === null) {
-      return arr;
+  const onDrop = (id: number, dropResult: any) => {
+    let { removedIndex, addedIndex } = dropResult;
+    if (removedIndex !== null || addedIndex !== null) {
+      const tmpArr: Array<{
+        id: number;
+        name: string;
+        children: Array<{ label: string }>;
+      }> = [...data];
+      const column = tmpArr.filter((item) => item.id === id)[0];
+      const columnIdx = tmpArr.indexOf(column);
+
+      tmpArr[columnIdx].children = applyDrag(column.children, dropResult);
+
+      setData(tmpArr);
     }
-    const result = [...arr];
-    let itemToAdd = payload;
-    if (removedIndex !== null) {
-      itemToAdd = result.splice(removedIndex, 1)[0];
-    }
-    if (addedIndex !== null) {
-      result.splice(addedIndex, 0, itemToAdd);
-    }
-    return result;
   };
 
-  const onDrop = (dropResult: any) => {
-    const { removedIndex, addedIndex } = dropResult;
-    if (removedIndex !== null || addedIndex !== null) {
-      const list = onDrag(data as Array<any>, dropResult);
-      setData(list);
-    }
+  const getCardPayload = (id: number, index: number) => {
+    return data.filter((p: any) => p.id === id)[0].children[index - 1];
   };
 
   return (
-    <Box>
-      <ContainerBox onDrop={onDrop} groupName="a">
-        {data.map((item) => (
-          <DraggableBox key={item.label}>
-            <div>{item.label}</div>
-          </DraggableBox>
-        ))}
-      </ContainerBox>
-
-      <ContainerBox onDrop={onDrop} groupName="a">
-        {data.map((item) => (
-          <DraggableBox key={item.label}>
-            <div>{item.label}</div>
-          </DraggableBox>
-        ))}
-      </ContainerBox>
+    <Box sx={{ display: "flex" }}>
+      {data.map((item: { id: number; name: string; children: any[] }) => (
+        <Paper key={item.id} sx={{ p: 3, pt: 0, mr: 3 }}>
+          <ContainerBox
+            onDrop={(e: any) => onDrop(item.id, e)}
+            getChildPayload={(idx: number) => getCardPayload(item.id, idx)}
+            groupName="col"
+            dropPlaceholderAnimationDuration={200}
+          >
+            <h3>{item.name}</h3>
+            {item.children.map((column: any) => (
+              <DraggableBox key={column?.label}>
+                <Card>{column?.label}</Card>
+              </DraggableBox>
+            ))}
+          </ContainerBox>
+        </Paper>
+      ))}
     </Box>
   );
 };
