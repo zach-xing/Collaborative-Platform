896673f2075fca2be911e53fd8d87208fe9c908e
Zeekg <3193373997@qq.com>
2022-07-05 23:03:04 +0800

    feat(client): add 'clouddocument' style component

---
+++ apps/client/pages/clouddocument/clouddocument.style.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+import { TreeItem } from "@mui/lab";
+
+/**
+ * 自定义 TreeItem 的样式
+ */
+export const CustomTreeItem = styled(TreeItem)`
+  .MuiTreeItem-content {
+    padding: 10px 0;
+  }
+`;
--- apps/client/pages/clouddocument/index.tsx
+++ apps/client/pages/clouddocument/index.tsx
@@ -1,10 +1,96 @@
 import React from "react";
+import TreeView from "@mui/lab/TreeView";
+import TreeItem from "@mui/lab/TreeItem";
+import { MdFolder, MdFolderOpen, MdInsertDriveFile } from "react-icons/md";
+import { Box, Typography } from "@mui/material";
+import ScrollBox from "../../components/ScrollBox";
+import FolderShow from "./components/FolderShow";
+import { CustomTreeItem } from "./clouddocument.style";
+
+type folderType = {
+  id: string;
+  label: string;
+  type: "file" | "folder";
+  children?: folderType[];
+};
+
+const mockData: Array<folderType> = [
+  {
+    id: "1",
+    type: "folder",
+    label: "文件1",
+    children: [
+      { id: "1-1", type: "file", label: "文件1-1" },
+      { id: "1-2", type: "file", label: "文件1-2" },
+    ],
+  },
+  {
+    id: "2",
+    type: "folder",
+    label: "文件2",
+    children: [{ id: "2-1", type: "file", label: "文件2-2" }],
+  },
+];
+
+const renderNode = (data: folderType) => {
+  if (data.children) {
+    return (
+      <CustomTreeItem key={data.id} nodeId={data.id} label={data.label}>
+        {data.children.map((col) => renderNode(col))}
+      </CustomTreeItem>
+    );
+  } else {
+    return (
+      <TreeItem
+        key={data.id}
+        nodeId={data.id}
+        label={data.label}
+        icon={<MdInsertDriveFile />}
+      />
+    );
+  }
+};
 
 /**
  * 云文档
  */
 const Clouddocument = () => {
-  return <div>Clouddocument</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        height: "100%",
+        width: "100%",
+        backgroundColor: "white",
+      }}
+    >
+      <Box
+        sx={{
+          flex: 2,
+          p: 1,
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Typography sx={{ flex: 1 }} variant="h6">
+          云文档
+        </Typography>
+
+        <ScrollBox sx={{ flex: 14 }}>
+          <TreeView
+            defaultCollapseIcon={<MdFolderOpen />}
+            defaultExpandIcon={<MdFolder />}
+          >
+            {mockData.map((column) => renderNode(column))}
+          </TreeView>
+        </ScrollBox>
+      </Box>
+
+      <Box sx={{ flex: 8, p: 1, pl: 3 }}>
+        <FolderShow />
+      </Box>
+    </Box>
+  );
 };
 
 export default Clouddocument;
