c44832b39bb185456c4f97fd250c68cad4c63155
Zeekg <3193373997@qq.com>
2022-07-08 17:23:13 +0800

    style(client): update 'index' page style

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -1,7 +1,28 @@
+import { Typography } from "@douyinfe/semi-ui";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>sdf</div>;
+  return (
+    <div
+      style={{
+        height: "100%",
+        backgroundColor: "white",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography.Title style={{ fontSize: "5rem" }}>
+        团队协作与管理平台
+      </Typography.Title>
+      <div style={{ marginTop: "30px" }}>
+        <Typography.Title style={{ fontSize: "2rem" }}>
+          团队协作与管理平台
+        </Typography.Title>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
