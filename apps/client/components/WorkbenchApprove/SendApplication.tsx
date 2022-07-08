833943b7cc0eb4c99f4efb1dad0c7a324d5d0c5d
Zeekg <3193373997@qq.com>
2022-07-08 15:41:02 +0800

    feat(client): add 'Modal' componet in 'workbench' page

--- apps/client/components/WorkbenchApprove/SendApplication.tsx
+++ apps/client/components/WorkbenchApprove/SendApplication.tsx
@@ -1,5 +1,11 @@
-import { IconClock, IconDesktop, IconExit } from "@douyinfe/semi-icons";
-import { Card, Col, Row, Typography } from "@douyinfe/semi-ui";
+import React from "react";
+import {
+  IconClock,
+  IconDesktop,
+  IconExit,
+  IconSendStroked,
+} from "@douyinfe/semi-icons";
+import { Button, Card, Col, Modal, Row, Typography } from "@douyinfe/semi-ui";
 
 const HeadingComp = (props: { iconComp: React.ReactNode; text: string }) => {
   return (
@@ -16,54 +22,87 @@ const HeadingComp = (props: { iconComp: React.ReactNode; text: string }) => {
  * 显示发送申请的组件
  */
 const SendApplication = () => {
+  const [visible, setVisible] = React.useState(false);
+  const onClose = () => {
+    setVisible(false);
+  };
+
   return (
-    <Row gutter={[16, 16]}>
-      <Col span={8}>
-        <Card
-          title={
-            <HeadingComp
-              iconComp={<IconClock size="extra-large" />}
-              text={"请假"}
-            />
-          }
-          shadows="hover"
-          headerLine={false}
-          headerStyle={{ backgroundColor: "lightblue" }}
-        >
-          请假申请
-        </Card>
-      </Col>
-      <Col span={8}>
-        <Card
-          title={
-            <HeadingComp
-              iconComp={<IconDesktop size="extra-large" />}
-              text={"加班"}
-            />
-          }
-          shadows="hover"
-          headerLine={false}
-          headerStyle={{ backgroundColor: "lightyellow" }}
-        >
-          加班申请
-        </Card>
-      </Col>
-      <Col span={8}>
-        <Card
-          title={
-            <HeadingComp
-              iconComp={<IconExit size="extra-large" />}
-              text={"外出"}
-            />
-          }
-          shadows="hover"
-          headerLine={false}
-          headerStyle={{ backgroundColor: "lightgreen" }}
-        >
-          外出申请
-        </Card>
-      </Col>
-    </Row>
+    <>
+      <Row gutter={[16, 16]}>
+        <Col span={8}>
+          <Card
+            title={
+              <HeadingComp
+                iconComp={<IconClock size="extra-large" />}
+                text={"请假"}
+              />
+            }
+            headerLine={false}
+            headerStyle={{ backgroundColor: "lightblue" }}
+            headerExtraContent={
+              <Button
+                icon={<IconSendStroked />}
+                onClick={() => setVisible(true)}
+              />
+            }
+          >
+            请假申请
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card
+            title={
+              <HeadingComp
+                iconComp={<IconDesktop size="extra-large" />}
+                text={"加班"}
+              />
+            }
+            headerLine={false}
+            headerStyle={{ backgroundColor: "lightyellow" }}
+            headerExtraContent={
+              <Button
+                icon={<IconSendStroked />}
+                onClick={() => setVisible(true)}
+              />
+            }
+          >
+            加班申请
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card
+            title={
+              <HeadingComp
+                iconComp={<IconExit size="extra-large" />}
+                text={"外出"}
+              />
+            }
+            headerLine={false}
+            headerStyle={{ backgroundColor: "lightgreen" }}
+            headerExtraContent={
+              <Button
+                icon={<IconSendStroked />}
+                onClick={() => setVisible(true)}
+              />
+            }
+          >
+            外出申请
+          </Card>
+        </Col>
+      </Row>
+
+      <Modal
+        title="全屏对话框标题"
+        fullScreen
+        visible={visible}
+        onOk={onClose}
+        onCancel={onClose}
+      >
+        <p>This is a full screen modal</p>
+        <p>More content...</p>
+      </Modal>
+    </>
   );
 };
 
--- apps/client/components/WorkbenchReport/SendReport.tsx
+++ apps/client/components/WorkbenchReport/SendReport.tsx
@@ -1,53 +1,92 @@
-import { Row, Col, Card } from "@douyinfe/semi-ui";
+import { IconSendStroked } from "@douyinfe/semi-icons";
+import { Row, Col, Card, Button, Modal } from "@douyinfe/semi-ui";
 import React from "react";
 
 /**
  * 发送汇报 组件
  */
 const SendReport = () => {
+  const [visible, setVisible] = React.useState(false);
+  const onClose = () => {
+    setVisible(false);
+  };
+
   return (
-    <Row gutter={[16, 16]}>
-      <Col span={6}>
-        <Card
-          title="日报"
-          shadows="hover"
-          headerLine={false}
-          headerStyle={{ backgroundColor: "lightblue" }}
-        >
-          汇报日报
-        </Card>
-      </Col>
-      <Col span={6}>
-        <Card
-          title="周报"
-          shadows="hover"
-          headerLine={false}
-          headerStyle={{ backgroundColor: "lightyellow" }}
-        >
-          汇报周报
-        </Card>
-      </Col>
-      <Col span={6}>
-        <Card
-          title="月报"
-          shadows="hover"
-          headerLine={false}
-          headerStyle={{ backgroundColor: "lightsalmon" }}
-        >
-          汇报月报
-        </Card>
-      </Col>
-      <Col span={6}>
-        <Card
-          title="自由汇报"
-          shadows="hover"
-          headerLine={false}
-          headerStyle={{ backgroundColor: "lightgreen" }}
-        >
-          自由汇报
-        </Card>
-      </Col>
-    </Row>
+    <>
+      <Row gutter={[16, 16]}>
+        <Col span={6}>
+          <Card
+            title="日报"
+            headerLine={false}
+            headerStyle={{ backgroundColor: "lightblue" }}
+            headerExtraContent={
+              <Button
+                icon={<IconSendStroked />}
+                onClick={() => setVisible(true)}
+              />
+            }
+          >
+            汇报日报
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card
+            title="周报"
+            headerLine={false}
+            headerStyle={{ backgroundColor: "lightyellow" }}
+            headerExtraContent={
+              <Button
+                icon={<IconSendStroked />}
+                onClick={() => setVisible(true)}
+              />
+            }
+          >
+            汇报周报
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card
+            title="月报"
+            headerLine={false}
+            headerStyle={{ backgroundColor: "lightsalmon" }}
+            headerExtraContent={
+              <Button
+                icon={<IconSendStroked />}
+                onClick={() => setVisible(true)}
+              />
+            }
+          >
+            汇报月报
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card
+            title="自由汇报"
+            headerLine={false}
+            headerStyle={{ backgroundColor: "lightgreen" }}
+            headerExtraContent={
+              <Button
+                icon={<IconSendStroked />}
+                onClick={() => setVisible(true)}
+              />
+            }
+          >
+            自由汇报
+          </Card>
+        </Col>
+      </Row>
+
+      <Modal
+        title="全屏对话框标题"
+        fullScreen
+        visible={visible}
+        onOk={onClose}
+        onCancel={onClose}
+      >
+        <p>This is a full screen modal</p>
+        <p>More content...</p>
+      </Modal>
+    </>
   );
 };
 
