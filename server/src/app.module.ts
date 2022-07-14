5e4bd4e081b6913821a3037648e2e46ab5d94f7b
Zeekg <3193373997@qq.com>
2022-07-14 19:26:00 +0800

    feat(server): add websocket chat module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './resources/user/user.module';
 import { ReportModule } from './resources/report/report.module';
+import { ChatModule } from './resources/chat/chat.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, ReportModule],
+  imports: [PrismaModule, UserModule, ReportModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/resources/chat/chat.gateway.ts
@@ -0,0 +1,58 @@
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { ChatService } from './chat.service';
+import { CreateChatDto } from './dto/create-chat.dto';
+import { UpdateChatDto } from './dto/update-chat.dto';
+
+@WebSocketGateway({
+  path: '/chat',
+  cors: {
+    origin: /.*/,
+    credentials: true,
+  },
+})
+export class ChatGateway {
+  @WebSocketServer() private ws: Server;
+
+  constructor(private readonly chatService: ChatService) {}
+
+  // 初始化的时候
+  afterInit() {
+    console.log('Chat WebSocket Initialized...');
+  }
+
+  @SubscribeMessage('message')
+  message() {
+    console.log('message');
+  }
+
+  @SubscribeMessage('createChat')
+  create(@MessageBody() createChatDto: CreateChatDto) {
+    return this.chatService.create(createChatDto);
+  }
+
+  @SubscribeMessage('findAllChat')
+  findAll() {
+    return this.chatService.findAll();
+  }
+
+  @SubscribeMessage('findOneChat')
+  findOne(@MessageBody() id: number) {
+    return this.chatService.findOne(id);
+  }
+
+  @SubscribeMessage('updateChat')
+  update(@MessageBody() updateChatDto: UpdateChatDto) {
+    return this.chatService.update(updateChatDto.id, updateChatDto);
+  }
+
+  @SubscribeMessage('removeChat')
+  remove(@MessageBody() id: number) {
+    return this.chatService.remove(id);
+  }
+}
