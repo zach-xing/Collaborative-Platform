f0f8f9fc59465f358df36fd3b491862feb2569b2
Zeekg <3193373997@qq.com>
2022-07-17 11:46:07 +0800

    fix(client): fix Hydration error

--- client/layout/Header.tsx
+++ client/layout/Header.tsx
@@ -7,94 +7,94 @@ import Login from "../components/Login";
 import Register from "../components/Register";
 
 const Header = () => {
-  const { route, push } = useRouter();
+  const router = useRouter();
+  // 1 表示在首页时登录状态、2表示不在首页时登录状态、3表示未登录
+  const [renderEl, setRenderEl] = React.useState<1 | 2 | 3>(1);
   const [loginVisible, setLoginVisible] = React.useState(false);
   const [registerVisible, setRegisterVisible] = React.useState(false);
 
-  let renderComp;
-  if (isLogin()) {
-    if (route === "/") {
-      // 已经登录情况下访问主页
-      renderComp = (
-        <>
-          <Button
-            theme="borderless"
-            type="primary"
-            onClick={() => push("/chat")}
-            style={{ marginRight: 8 }}
-          >
-            进入空间
-          </Button>
-          <Avatar color="orange" size="small">
-            YJ
-          </Avatar>
-        </>
-      );
+  React.useEffect(() => {
+    if (isLogin()) {
+      if (router.route === "/") {
+        setRenderEl(1);
+      } else {
+        setRenderEl(2);
+      }
     } else {
-      // 已经登录情况下进入空间
-      renderComp = (
-        <>
-          <Button
-            theme="borderless"
-            icon={<IconBell size="large" />}
-            style={{
-              color: "var(--semi-color-text-2)",
-              marginRight: "12px",
-            }}
-          />
-          <Button
-            theme="borderless"
-            icon={<IconHelpCircle size="large" />}
-            style={{
-              color: "var(--semi-color-text-2)",
-              marginRight: "12px",
-            }}
-          />
-          <Avatar color="orange" size="small">
-            YJ
-          </Avatar>
-        </>
-      );
+      setRenderEl(3);
     }
-  } else {
-    if (route === "/") {
-      // 未登录的情况下访问主页
-      renderComp = (
-        <>
-          <Button
-            theme="borderless"
-            type="primary"
-            style={{ marginRight: 8 }}
-            onClick={() => setRegisterVisible(true)}
-          >
-            注册
-          </Button>
-          <Button
-            theme="borderless"
-            type="primary"
-            style={{ marginRight: 8 }}
-            onClick={() => setLoginVisible(true)}
-          >
-            登录
-          </Button>
-
-          <Login visible={loginVisible} setVisible={setLoginVisible} />
-          <Register visible={registerVisible} setVisible={setRegisterVisible} />
-        </>
-      );
-    } else {
-      /* 在未登录的情况下访问其他页面会被拦截做鉴权，会被重定向到首页 */
-    }
-  }
+  }, [router.route]);
 
   return (
-    <Nav mode="horizontal">
-      <Nav.Header>
-        <IconSemiLogo style={{ fontSize: 36 }} />
-      </Nav.Header>
+    <>
+      <Nav mode="horizontal">
+        <Nav.Header>
+          <IconSemiLogo style={{ fontSize: 36 }} />
+        </Nav.Header>
 
-      <Nav.Footer>{renderComp}</Nav.Footer>
-    </Nav>
+        <Nav.Footer>
+          {renderEl === 1 ? (
+            <>
+              <Button
+                theme="borderless"
+                type="primary"
+                onClick={() => router.push("/chat")}
+                style={{ marginRight: 8 }}
+              >
+                进入空间
+              </Button>{" "}
+              <Avatar color="orange" size="small">
+                YJ
+              </Avatar>
+            </>
+          ) : renderEl === 2 ? (
+            <>
+              <Button
+                theme="borderless"
+                icon={<IconBell size="large" />}
+                style={{
+                  color: "var(--semi-color-text-2)",
+                  marginRight: "12px",
+                }}
+              />
+              <Button
+                theme="borderless"
+                icon={<IconHelpCircle size="large" />}
+                style={{
+                  color: "var(--semi-color-text-2)",
+                  marginRight: "12px",
+                }}
+              />
+              <Avatar color="orange" size="small">
+                YJ
+              </Avatar>
+            </>
+          ) : (
+            <>
+              <Button
+                theme="borderless"
+                type="primary"
+                style={{ marginRight: 8 }}
+                onClick={() => setRegisterVisible(true)}
+              >
+                注册
+              </Button>
+              <Button
+                theme="borderless"
+                type="primary"
+                style={{ marginRight: 8 }}
+                onClick={() => setLoginVisible(true)}
+              >
+                登录
+              </Button>
+            </>
+          )}
+        </Nav.Footer>
+      </Nav>
+
+      <Login visible={loginVisible} setVisible={setLoginVisible} />
+      <Register visible={registerVisible} setVisible={setRegisterVisible} />
+    </>
   );
 };
 
--- client/layout/index.tsx
+++ client/layout/index.tsx
@@ -10,37 +10,48 @@ const LayoutComp = ({ children }: { children: React.ReactNode }) => {
   const { Header, Sider, Content } = Layout;
   const router = useRouter();
 
-  if (router.route === "/") {
-    return (
-      <Layout style={{ height: "100%" }}>
-        <Header>
-          <HeaderBox />
-        </Header>
+  // if (router.route === "/") {
+  //   return (
+  //     <Layout style={{ height: "100%" }}>
+  //       <Header>
+  //         <HeaderBox />
+  //       </Header>
+  //       <Content>{children}</Content>
+  //     </Layout>
+  //   );
+  // } else {
+  //   // '/chat'、'/task' 等
+  //   if (!isLogin()) {
+  //     // 在需要鉴权的页面 and 未登录
+  //     return <Redirect url="/" />;
+  //   } else {
+  //     // 在需要鉴权的页面 and 已登录
+  //     return (
+
+  //     );
+  //   }
+  // }
+  React.useEffect(() => {
+    if (!isLogin()) {
+      router.replace("/");
+    }
+  }, [router, router.route]);
+
+  return (
+    <Layout style={{ height: "100%" }}>
+      <Header>
+        <HeaderBox />
+      </Header>
+      <Layout style={{ height: "calc(100vh - 60px)" }}>
+        {router.route !== "/" && (
+          <Sider>
+            <SiderBox />
+          </Sider>
+        )}
         <Content>{children}</Content>
       </Layout>
-    );
-  } else {
-    // '/chat'、'/task' 等
-    if (!isLogin()) {
-      // 在需要鉴权的页面 and 未登录
-      return <Redirect url="/" />;
-    } else {
-      // 在需要鉴权的页面 and 已登录
-      return (
-        <Layout style={{ height: "100%" }}>
-          <Header>
-            <HeaderBox />
-          </Header>
-          <Layout style={{ height: "calc(100vh - 60px)" }}>
-            <Sider>
-              <SiderBox />
-            </Sider>
-            <Content>{children}</Content>
-          </Layout>
-        </Layout>
-      );
-    }
-  }
+    </Layout>
+  );
 };
 
 export default LayoutComp;
