42cec10bf657fda0263602b53f357733235a73ff
Zeekg <3193373997@qq.com>
2022-07-18 23:32:00 +0800

    feat(server): update '/chat-user/:id' request

--- server/src/resources/chat-user/chat-user.service.ts
+++ server/src/resources/chat-user/chat-user.service.ts
@@ -9,25 +9,52 @@ export class ChatUserService {
    * @param id
    */
   async findUsersById(id: string) {
-    const chatUsers = await this.prisma.friend.findUnique({
-      where: { userId: id },
-    });
-    if (chatUsers.friend_list === '') {
-      return [];
-    }
-    const userIdArr = chatUsers.friend_list.split(',');
-    const arr = await this.prisma.user.findMany({
+    // 找 chatRoom 的 userIds 包含 id 的记录
+    const chatRoomIds = await this.prisma.chatRoom.findMany({
       where: {
-        id: {
-          in: userIdArr,
+        userIds: {
+          contains: id,
         },
       },
-      select: {
-        id: true,
-        name: true,
-        email: true,
+      select: { id: true },
+    });
+
+    if (chatRoomIds.length === 0) {
+      return [];
+    }
+    // 将 Array<{id: string}> 转变成 Array<string>
+    const arr: string[] = chatRoomIds.map((item) => item.id);
+    // userIdsArr.userIds 里面都是形如 ["id1,id2,id3", "id1,id4"] 之类的数据
+    const userIdsArr = await this.prisma.chatRoom.findMany({
+      where: {
+        id: { in: arr },
       },
     });
-    return arr;
+
+    const res: any[] = [];
+    let tmp;
+    // 最后将值变成 Array<{charRoomId: string; chatUsers: {id: string;name: string;email: string;}[]}>
+    for (let i = 0; i < userIdsArr.length; i += 1) {
+      tmp = userIdsArr[i].userIds;
+      // 用户列表
+      const chatUsers = await this.prisma.user.findMany({
+        where: {
+          id: {
+            in: tmp.split(','),
+          },
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      });
+      res.push({
+        charRoomId: userIdsArr[i].id,
+        chatUsers,
+      });
+    }
+
+    return res;
   }
 }
