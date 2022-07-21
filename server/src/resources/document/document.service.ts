c02f3d428a1bd82c630057bafc759e660ecc2127
Zeekg <3193373997@qq.com>
2022-07-21 21:14:18 +0800

    feat(server): add multiple socket debounce

--- server/src/resources/document/document.service.ts
+++ server/src/resources/document/document.service.ts
@@ -7,10 +7,13 @@ import {
 } from '@hocuspocus/server';
 import { TiptapTransformer } from '@hocuspocus/transformer';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as Y from 'yjs';
 
 @Injectable()
 export class DocumentService {
   server: typeof Server;
+  timers: Map<string, { timeout: NodeJS.Timeout; start: number }> = new Map();
+  maxDebounceTime = 10000; // 最大的防抖时间限制
 
   constructor(private prisma: PrismaService) {
     this.initServer();
@@ -54,8 +57,10 @@ export class DocumentService {
   }
 
   async onChange(data: onChangePayload) {
-    const { requestParameters } = data;
-    console.log('onChange');
+    const { requestParameters, update } = data;
+    const node = TiptapTransformer.fromYdoc(data.document);
+    const state = Buffer.from(Y.encodeStateAsUpdate(data.document));
+    console.log(node);
   }
 
   async onDisconnect(data) {
@@ -64,5 +69,28 @@ export class DocumentService {
     const docType = requestParameters.get('docType');
     const userId = requestParameters.get('userId');
     console.log('onDisconnect');
+  }
+
+  // 防抖（防止一个连接影响另外一个连接，所以使用了 Map 作为数据结构）
+  private debounce(id: string, func: () => void, delay: number) {
+    const old = this.timers.get(id);
+    const start = old?.start || Date.now();
+
+    // 真正的执行
+    const run = () => {
+      this.timers.delete(id);
+      func();
+    };
+
+    if (old?.timeout) {
+      clearTimeout(old.timeout);
+    }
+
+    // 防止一直输入而无法执行 func 的情况，限制了最大防抖时间
+    if (Date.now() - start >= this.maxDebounceTime) {
+      run();
+    }
+
+    this.timers.set(id, { start, timeout: setTimeout(run, 2000) });
   }
 }
